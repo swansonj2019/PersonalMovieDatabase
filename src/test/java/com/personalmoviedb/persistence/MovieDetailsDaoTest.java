2ddd476f2bc9b71968f57d82d78a99ca2eeecf4a
swansonj2019 <jswanson@madisoncollege.edu>
2020-03-11 19:06:47 -0500

    Added additonal Assertions and updated Week 8 Screenshot

--- src/test/java/com/personalmoviedb/persistence/MovieDetailsDaoTest.java
+++ src/test/java/com/personalmoviedb/persistence/MovieDetailsDaoTest.java
@@ -10,5 +10,9 @@ class MovieDetailsDaoTest {
     void getDetailsSuccess() {
         MovieDetailsDao dao = new MovieDetailsDao();
         assertEquals("Joker", dao.getDetails().getTitle());
+        assertEquals("http://www.jokermovie.net/", dao.getDetails().getHomepage());
+        assertEquals("During the 1980s, a failed stand-up comedian is driven insane and turns to a life of" +
+                    " crime and chaos in Gotham City while becoming an infamous psychopathic crime figure.",
+                dao.getDetails().getOverview());
     }
 }
